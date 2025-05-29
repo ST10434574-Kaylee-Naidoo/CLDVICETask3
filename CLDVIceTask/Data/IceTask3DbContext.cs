72aed55f0074c42724f33ba36d808a01f324597a
st10434574 <st10434574@vcconnect.edu.za>
2025-05-29 04:25:58 +0200

    new visual studio file added and all steps taken to add CRUD functionality from one table

---
+++ CLDVIceTask/Data/IceTask3DbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using CLDVIceTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CLDVIceTask.Data;
+
+public partial class IceTask3DbContext : DbContext
+{
+    public IceTask3DbContext()
+    {
+    }
+
+    public IceTask3DbContext(DbContextOptions<IceTask3DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+ 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__User__1788CCACDB9D4027");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.ProfilePicture)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
